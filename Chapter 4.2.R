144fe2748e7ece471e222c79066289a613d93608
wormiss <woimiss@gmail.com>
2017-10-10 21:43:52 +0100

    More advanced spatial operations

---
+++ Chapter 4.2.R
@@ -0,0 +1,75 @@
+library(sf)
+library(tidyverse)
+library(raster)
+library(spData)
+
+# Spatial subsetting
+
+# create Africa dataset
+africa_wgs = world[world$continent == "Africa",]
+africa = st_transform(africa_wgs, crs = 32630)
+
+# create circle centred on point where GMT crosses equator
+center = st_sf(geometry = st_sfc(st_point(c(0, 0)), crs = 4326))
+buff = st_buffer(x = center, dist = 20)
+buff= st_transform(buff, 32630)
+
+# intersect query
+africa_buff = africa[buff, ]
+plot(africa_buff["pop"])
+plot(buff, add = TRUE)
+
+# Another method of intersecting
+sel_buff = st_intersects(x = africa, y = buff, sparse = FALSE)
+africa_buff2 = africa[sel_buff,]
+plot(africa_buff2["lifeExp"])
+plot(buff, add = TRUE)
+
+# a third way of intersecting
+africa_buff3 = filter(africa, sel_buff)
+plot(africa_buff3["area_km2"])
+plot(buff, add = TRUE)
+
+# test whether 3 results are identical
+identical(x = africa_buff, y = africa_buff2)
+identical(x = africa_buff, y = africa_buff3) # FALSE because dplyr changes row names
+
+# create some topological data for understanding theire relationships
+a1 = st_polygon(list(rbind(c(-1, -1), c(1, -1), c(1, 1), c(-1, -1))))
+a = st_sfc(a1)
+l1 = st_linestring(x = matrix(c(-1, -1, -0.5, 1), , 2))
+l = st_sfc(l1)
+p_matrix = matrix(c(0.5, 1, -1, 0, 0, 1, 0.5, 1), ncol = 2)
+p_multi = st_multipoint(x = p_matrix)
+p = st_sf(st_cast(st_sfc(p_multi), "POINT"))
+
+# draw shapes
+plot(a)
+plot(l, add = TRUE)
+plot(p, add = TRUE)
+
+# find which objects intersect: 1 means yes, 0 means no
+st_intersects(p, a)
+st_intersects(p, a, sparse = FALSE) # results in a logical vector that is more useful
+p[st_intersects(p, a, sparse = FALSE),]
+
+# other topological operations
+st_within(p, a, sparse = FALSE)
+st_touches(p, a, sparse = FALSE) # only returns objects touching border
+st_is_within_distance(p,a, dist = 0.9) # can only return sparse matrix
+
+# spatial joining and aggregation
+urb = urban_agglomerations %>%
+  filter(year == 2020) %>% # 2020 population
+  top_n(n = 3, wt = population_millions) # top 3
+asia = world %>%
+  filter(continent == "Asia")
+
+joined = st_join(x = asia, y = urb) # default left join and intersect
+joined[!is.na(joined$population_millions),]
+plot(joined["population_millions"])
+
+joined = st_join(x = asia, y = urb, left = FALSE) # inner join
+joined[!is.na(joined$population_millions),]
+plot(joined["population_millions"])
+
